732cf4aa0244e8beb27483b4bab20113614362eb
MarkKoz <KozlovMark@gmail.com>
2020-03-21 20:38:59 -0700

    Use selector event loop on Windows

--- testbot/__init__.py
+++ testbot/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 import sys
@@ -15,3 +16,7 @@ if DEBUG != 2:
     logging.getLogger('asyncio').setLevel(logging.ERROR)
     logging.getLogger('discord').setLevel(logging.ERROR)
     logging.getLogger('websockets').setLevel(logging.ERROR)
+
+if os.name == 'nt':
+    # Required by aiodns.
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())

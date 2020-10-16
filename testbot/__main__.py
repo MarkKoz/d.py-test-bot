e3f9c1cd720f0d58fb53f41b527d9170d30fd675
MarkKoz <KozlovMark@gmail.com>
2020-10-15 21:08:59 -0700

    Update discord.py to 1.5

--- testbot/__main__.py
+++ testbot/__main__.py
@@ -1,7 +1,7 @@
+import logging
 import os
 
-import asyncio
-import logging
+import discord
 
 from testbot.bot import TestBot
 from testbot.utils import extensions
@@ -12,7 +12,8 @@ bot = TestBot(
     command_prefix='!',
     description='Discord.py Test Bot',
     pm_help=None,
-    help_attrs=dict(hidden=True)
+    help_attrs=dict(hidden=True),
+    intents=discord.Intents.all()
 )
 
 for extension in extensions.get_extensions():
